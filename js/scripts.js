b1fbead6501d190f0f3a9ef32e58fcd46dbe488e
SrGodoy <115853981+SrGodoy@users.noreply.github.com>
2024-01-27 22:36:50 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,97 @@
+class Todo {
+    constructor() {
+       this.totalTasks = document.querySelectorAll('.task').length; 
+    }
+    addTask(taskText) {
+        
+        //clona template
+        let template = document.querySelector('.task').cloneNode(true);
+        // remove classe hide
+        template.classList.remove('hide');
+        //manipular texto
+        let templateText = template.querySelector('.task-title');
+        templateText.textContent = taskText;
+
+        let list = document.querySelector('#tasks-container');
+
+        //inserir na lista
+        list.appendChild(template);
+
+        //adciona evento as tasks
+        this.addEvents();
+
+        this.checkTasks('add');
+    }
+    removeTask(task) {
+          //achar o elemento pai
+          let parentEl = task.parentElement;
+
+          //remover da lista
+          parentEl.remove();
+          
+          this.checkTasks('remove');
+    }
+
+    completeTask(task) {
+     
+        //adciona a classe de done
+        task.classList.add('done');
+
+    }
+
+
+    addEvents() {
+
+        let removeBtns = document.querySelectorAll('.fa-trash');
+        let removeBtn = removeBtns[removeBtns.length - 1];
+        let doneBtns = document.querySelectorAll('.fa-check');
+        let doneBtn = doneBtns[doneBtns.length - 1];
+
+        // adcionar evento de remover
+        removeBtn.addEventListener('click', function() {
+            todo.removeTask(this);
+        });
+
+        //adcionar evento de completar tarefa
+        doneBtn.addEventListener('click', function() {
+            todo.completeTask(this);
+        });
+    }
+    checkTasks(command) {
+        let msg = document.querySelector('#empty-tasks');
+
+        //lógica de adcionar ou remover tasks
+        if(command === 'add') {
+            this.totalTasks += 1; 
+        }else if(command === 'remove' ) {
+            this.totalTasks -= 1;
+        }
+
+        //checa se tem mais de uma task e adiciona ou remove a classe
+        if(this.totalTasks == 1) {
+            msg.classList.remove('hide');
+        }else {
+            msg.classList.add('hide');
+        }
+
+    }
+}
+
+let todo = new Todo() ;
+
+//events
+let addBtn = document.querySelector('#add');
+
+addBtn.addEventListener('click', function(e) {
+    
+    e.preventDefault();
+
+    let taskText = document.querySelector('#task');
+
+    if(taskText.value != '') {
+    todo.addTask(taskText.value);
+    }
+    // limpa campo de texto
+    taskText.value = '';
+
+});
